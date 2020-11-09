bb4c207d4d2a0a8f654b7419fcf763a7b3f75e59
AnnaStGermain <anna.stgermain885@gmail.com>
2020-11-08 21:19:34 -0500

    Updated some collections

--- imports/api/mineral-info.js
+++ imports/api/mineral-info.js
@@ -2,4 +2,82 @@ import { Mongo } from 'meteor/mongo';
 
 const mi = new Mongo.Collection('mineral_info');
 
+mi.insert({
+    mineral_info_id: "0",
+    mineral_family_name: "Metals",
+    mineral_group_name: "Native Elements"
+});
+
+mi.insert({
+    mineral_info_id: "1",
+    mineral_family_name: "Semimetals",
+    mineral_group_name: "Native Elements"
+});
+
+mi.insert({
+    mineral_info_id: "2",
+    mineral_family_name: "Nonmetals",
+    mineral_group_name: "Native Elements"
+});
+
+mi.insert({
+    mineral_info_id: "3",
+    mineral_family_name: "",
+    mineral_group_name: "Sulfides"
+});
+
+mi.insert({
+    mineral_info_id: "4",
+    mineral_family_name: "",
+    mineral_group_name: "Sulfosalts"
+});
+
+mi.insert({
+    mineral_info_id: "5",
+    mineral_family_name: "",
+    mineral_group_name: "Oxides"
+});
+
+mi.insert({
+    mineral_info_id: "6",
+    mineral_family_name: "",
+    mineral_group_name: "Hydroxides"
+});
+
+mi.insert({
+    mineral_info_id: "7",
+    mineral_family_name: "",
+    mineral_group_name: "Halides"
+});
+
+mi.insert({
+    mineral_info_id: "8",
+    mineral_family_name: "",
+    mineral_group_name: "Carbonates"
+});
+
+mi.insert({
+    mineral_info_id: "9",
+    mineral_family_name: "",
+    mineral_group_name: "Borates"
+});
+
+mi.insert({
+    mineral_info_id: "10",
+    mineral_family_name: "",
+    mineral_group_name: "Sulfates"
+});
+
+mi.insert({
+    mineral_info_id: "11",
+    mineral_family_name: "",
+    mineral_group_name: "Phosphates"
+});
+
+mi.insert({
+    mineral_info_id: "12",
+    mineral_family_name: "",
+    mineral_group_name: "Silicates"
+});
+
 export { mi as MineralInfo }
