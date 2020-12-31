4f8ab3420fad43dcf09ce258dbe0735882e8b00c
n0tanna <anna.stgermain885@gmail.com>
2020-12-31 03:54:48 -0500

    added data

--- imports/ui/components/species/species.js
+++ imports/ui/components/species/species.js
@@ -7,17 +7,34 @@ import 'bootstrap/dist/css/bootstrap.css'
 let domain = "";
 let kingdom = "";
 let phylum = "";
+let classes = "";
+let order = "";
+let family = "";
+let genus = "";
+
+let kButton = new ReactiveVar();
+let dButton = new ReactiveVar();
+let pButton = new ReactiveVar();
+let cButton = new ReactiveVar();
+let oButton = new ReactiveVar();
+let fButton = new ReactiveVar();
+let gButton = new ReactiveVar();
 
 let displayDomain = new ReactiveVar("yes");
 let kingdomHolder = new ReactiveVar();
 let domainHolder = new ReactiveVar();
 let phylumHolder = new ReactiveVar();
+let classHolder = new ReactiveVar();
+let orderHolder = new ReactiveVar();
+let familyHolder = new ReactiveVar();
+let genusHolder = new ReactiveVar();
 
 let dbHolder = new ReactiveArray();
 
-function loadDomains() {
-    dbHolder.clear();
-    let tempHolderStart = Species.find().fetch();
+function loadInfo(info, taxLevel, pluralInfo) {
+    if (info === "") {
+        dbHolder.clear();
+        let tempHolderStart = Species.find().fetch();
         let tempHolderDoms = new Array();
 
         tempHolderStart.forEach(element => tempHolderDoms.push(element.domains));
@@ -27,30 +44,26 @@ function loadDomains() {
                 dbHolder.push(domain);
             });
         });
-}
-
-function loadKingdoms(domain) {
-    dbHolder.forEach(function (domains) {
-        if (domains.domain === domain) {
-            let tempHolder = domains.kingdoms;
-            dbHolder.clear();
-            tempHolder.forEach(function (kingdoms) {
-                dbHolder.push(kingdoms);
-            });
-        }
-    });
-}
-
-function loadPhylums(kingdom) {
-    dbHolder.forEach(function (kingdoms) {
-        if (kingdoms.kingdom === kingdom) {
-            let tempHolder = kingdoms.phylums;
-            dbHolder.clear();
-            tempHolder.forEach(function (phylums) {
-                dbHolder.push(phylums);
-            });
-        }
-    });
+    }
+    else {
+        dbHolder.forEach(function (values) {
+            console.log(pluralInfo);
+            if (values[info] === taxLevel) {
+                let tempHolder = values[pluralInfo];
+                console.log(tempHolder);
+                dbHolder.clear();
+                tempHolder.forEach(function (innerValues) {
+                    if(innerValues[info] === "N/A" && !(innerValues[info] == "phylum")) {
+                        genusHolder.set("yes");
+                        console.log("ok");
+                    }
+                    else {
+                        dbHolder.push(innerValues);
+                    }
+                });
+            }
+        });
+    }
 }
 
 if (Meteor.isClient) {
@@ -65,6 +78,54 @@ if (Meteor.isClient) {
 
         domainValue: function () {
             return domainHolder.get();
+        },
+
+        phylumValue: function () {
+            return phylumHolder.get();
+        },
+
+        classValue: function () {
+            return classHolder.get();
+        },
+
+        orderValue: function () {
+            return orderHolder.get();
+        },
+
+        familyValue: function () {
+            return familyHolder.get();
+        },
+        
+        genusValue: function () {
+            return genusHolder.get();
+        },
+
+        buttonDomain: function () {
+            return dButton.get();
+        },
+
+        buttonKingdom: function () {
+            return kButton.get();
+        },
+
+        buttonPhylum: function () {
+            return pButton.get();
+        },
+
+        buttonClass: function () {
+            return cButton.get();
+        },
+
+        buttonOrder: function () {
+            return oButton.get();
+        },
+
+        buttonFamily: function () {
+            return fButton.get();
+        },
+
+        buttonGenus: function () {
+            return gButton.get();
         }
     });
 
@@ -85,7 +146,7 @@ if (Meteor.isClient) {
     });
 
     Template.phylumArea.helpers({
-        phylumNames: function() {
+        phylumNames: function () {
             return dbHolder.list();
         },
 
@@ -94,27 +155,159 @@ if (Meteor.isClient) {
         }
     });
 
+    Template.classArea.helpers({
+        classNames: function () {
+            return dbHolder.list();
+        },
+
+        phylumName: function () {
+            return phylum;
+        }
+    });
+
+    Template.orderArea.helpers({
+        orderNames: function () {
+            return dbHolder.list();
+        },
+
+        className: function () {
+            return classes;
+        }
+    });
+
+    Template.familyArea.helpers({
+        familyNames: function () {
+            return dbHolder.list();
+        },
+
+        orderName: function () {
+            return order;
+        }
+    });
+
+    Template.genusArea.helpers({
+        genusNames: function () {
+            return dbHolder.list();
+        },
+
+        familyName: function () {
+            return family;
+        }
+    });
+
+    Template.speciesArea.helpers({
+
+        genusName: function () {
+            return genus;
+        }
+    });
+
     Template.species.onCreated(function nice() {
-        loadDomains();
+        loadInfo("");
+        dButton.set("true");
     });
 
     Template.species.events({
         'click .domain': function () {
             displayDomain.set("yes");
             domain = "";
-            kingdom = "";
             domainHolder.set("");
             kingdomHolder.set("");
-            loadDomains();
+            
+            loadInfo("");
+
+            dButton.set("");
         },
 
         'click .kingdom': function () {
             kingdom = "";
             domainHolder.set(domain);
             kingdomHolder.set("");
+            phylumHolder.set("");
             dbHolder.clear();
-            loadDomains();
-            loadKingdoms(domain);
+
+            loadInfo("");
+            loadInfo("domain", domain, "kingdoms");
+
+            kButton.set("");
+        },
+
+        'click .phylum': function () {
+            phylum = "";
+            kingdomHolder.set(kingdom);
+            phylumHolder.set("");
+            dbHolder.clear();
+
+            loadInfo("");
+            loadInfo("domain", domain, "kingdoms");
+            loadInfo("kingdom", kingdom, "phylums");
+
+            pButton.set("");
+        },
+
+        'click .class': function () {
+            classes = "";
+            phylumHolder.set(phylum);
+            classHolder.set("");
+            dbHolder.clear();
+
+            loadInfo("");
+            loadInfo("domain", domain, "kingdoms");
+            loadInfo("kingdom", kingdom, "phylums");
+            loadInfo("phylum", phylum, "classes");
+
+            cButton.set("");
+        },
+
+        'click .order': function () {
+            order = "";
+            classHolder.set(classes);
+            orderHolder.set("");
+
+            dbHolder.clear();
+
+            loadInfo("");
+            loadInfo("domain", domain, "kingdoms");
+            loadInfo("kingdom", kingdom, "phylums");
+            loadInfo("phylum", phylum, "classes");
+            loadInfo("class", classes, "orders");
+
+            oButton.set("");
+        },
+
+        'click .family': function () {
+            family = "";
+            orderHolder.set(order);
+            familyHolder.set("");
+
+            dbHolder.clear();
+
+            loadInfo("");
+            loadInfo("domain", domain, "kingdoms");
+            loadInfo("kingdom", kingdom, "phylums");
+            loadInfo("phylum", phylum, "classes");
+            loadInfo("class", classes, "orders");
+            loadInfo("order", order, "families");
+
+            fButton.set("");
+        },
+
+        'click .genus': function () {
+            genus = "";
+            familyHolder.set(family);
+            genusHolder.set("");
+
+            dbHolder.clear();
+
+            loadInfo("");
+            loadInfo("domain", domain, "kingdoms");
+            loadInfo("kingdom", kingdom, "phylums");
+            loadInfo("phylum", phylum, "classes");
+            loadInfo("class", classes, "orders");
+            loadInfo("order", order, "families");
+            loadInfo("family", family, "genera");
+
+            gButton.set("");
         },
 
         'click .select': function (event) {
@@ -122,18 +315,61 @@ if (Meteor.isClient) {
                 domain = this.domain;
                 domainHolder.set(domain);
                 displayDomain.set("");
-                loadKingdoms(domain);
+
+                loadInfo("domain", domain, "kingdoms");
+
+                dButton.set("true");
             }
             else if (kingdom === "") {
                 kingdom = event.currentTarget.getAttribute("id");
                 domainHolder.set("");
                 kingdomHolder.set(kingdom);
-                loadPhylums(kingdom);
+
+                loadInfo("kingdom", kingdom, "phylums");
+
+                kButton.set("true");
             }
             else if (phylum === "") {
-                kingdomHolder.set("");
                 phylum = event.currentTarget.getAttribute("id");
+                kingdomHolder.set("");
                 phylumHolder.set(phylum);
+
+                loadInfo("phylum", phylum, "classes");
+
+                pButton.set("true");
+            }
+            else if (classes === "") {
+                classes = event.currentTarget.getAttribute("id");
+                phylumHolder.set("");
+                classHolder.set(classes);
+
+                loadInfo("class", classes, "orders");
+
+                cButton.set("true");
+            }
+            else if (order == "") {
+                order = event.currentTarget.getAttribute("id");
+                classHolder.set("");
+                orderHolder.set(order);
+
+                loadInfo("order", order, "families");
+
+                oButton.set("true");
+            }
+            else if (family == "") {
+                family = event.currentTarget.getAttribute("id");
+                orderHolder.set("");
+                familyHolder.set(order);
+
+                loadInfo("family", family, "genera");
+
+                fButton.set("true");
+            }
+            else if (genus == "") {
+                genus = event.currentTarget.getAttribute("id");
+                familyHolder.set("");
+                genusHolder.set(order);
+                gButton.set("true");
             }
         }
     });
